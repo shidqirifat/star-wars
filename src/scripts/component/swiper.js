127acd0ed5ce4ebec258e40bf54b26b05abece61
Shidqi <rifatpangestu87@gmail.com>
2023-08-02 17:19:02 +0700

    refactor: initialize swiper hero

---
+++ src/scripts/component/swiper.js
@@ -0,0 +1,38 @@
+import Swiper from "swiper";
+import { Autoplay, Pagination } from "swiper/modules";
+import "swiper/css/bundle";
+import "swiper/css/autoplay";
+import "swiper/css/pagination";
+
+const swiper = new Swiper(".swiper", {
+  speed: 400,
+  loop: true,
+  autoplay: {
+    pauseOnMouseEnter: true,
+    delay: 3000,
+    disableOnInteraction: false,
+  },
+  observer: true,
+  pagination: {
+    el: ".swiper-pagination",
+    type: "bullets",
+  },
+  breakpoints: {
+    220: {
+      spaceBetween: 16,
+      slidesPerView: 1.15,
+    },
+    550: {
+      spaceBetween: 20,
+      slidesPerView: 1.2,
+    },
+    880: {
+      spaceBetween: 24,
+      slidesPerView: 1.7,
+      centeredSlides: true,
+    },
+  },
+  modules: [Autoplay, Pagination],
+});
+
+export default swiper;
--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,33 +1,16 @@
-import Swiper from 'swiper';
-import { Autoplay, Pagination } from 'swiper/modules';
-import 'swiper/css/bundle';
-import 'swiper/css/autoplay';
-import 'swiper/css/pagination';
 import "./component/hero.js";
+import swiper from "./component/swiper.js";
 import "./component/card.js";
 
 const main = () => {
   document.addEventListener("DOMContentLoaded", async () => {
-    console.log('loaded');
-
-    new Swiper('.swiper', {
-      speed: 400,
-      spaceBetween: 24,
-      slidesPerView: 1.2,
-      centeredSlides: true,
-      loop: true,
-      autoplay: {
-        pauseOnMouseEnter: true,
-        delay: 3000,
-        disableOnInteraction: false
-      },
-      pagination: {
-        el: '.swiper-pagination',
-        type: 'bullets',
-      },
-      modules: [Autoplay, Pagination]
-    });
+    console.log("loaded");
   });
-}
+
+  window.addEventListener("resize", () => {
+    console.log("object");
+    swiper.update();
+  });
+};
 
 export default main;
